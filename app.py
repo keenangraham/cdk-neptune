dcda0c45454be8251aae11a50bb4d3bdba4918a6
Keenan Graham <ksgraham@stanford.edu>
2023-06-12 14:19:41 -0700

    Update

---
+++ app.py
@@ -0,0 +1,28 @@
+from aws_cdk import App
+from aws_cdk import CfnOutput
+from aws_cdk import Duration
+from aws_cdk import Environment
+from aws_cdk import Stack
+
+from constructs import Construct
+
+
+app = App()
+
+class NeptuneStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+
+neptune_stack = NeptuneStack(
+    app,
+    'NeptuneStack',
+    env=Environment(
+        account='618537831167',
+        region='us-west-2',
+    )
+)
+
+
+app.synth()

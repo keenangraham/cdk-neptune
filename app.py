35f8b31f368dddfdd2d16700dedbff9aea0b303c
Keenan Graham <ksgraham@stanford.edu>
2023-06-27 14:30:33 -0700

    Add Auth0 authentication to ALB

--- app.py
+++ app.py
@@ -29,9 +29,15 @@ from aws_cdk.aws_ecs_patterns import ApplicationListenerProps
 from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancerProps
 from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedTaskImageProps
 
+from aws_cdk.aws_elasticloadbalancingv2 import ApplicationTargetGroup
+from aws_cdk.aws_elasticloadbalancingv2 import ListenerAction
+from aws_cdk.aws_elasticloadbalancingv2 import ListenerCondition
+
 from aws_cdk.aws_certificatemanager import Certificate
 
 from aws_cdk.aws_route53 import HostedZone
+
+from aws_cdk.aws_secretsmanager import Secret
 
 from constructs import Construct
 
@@ -107,11 +113,6 @@ class NeptuneStack(Stack):
             container_name='client',
             command=['npm', 'run', 'client'],
             image=docker_image,
-           # port_mappings=[
-           #     PortMapping(
-           #         container_port=3000
-           #     )
-           # ],
             environment={
                     'NEPTUNE_ENDPOINT': write_address,
                     'NODE_ENV': 'production',
@@ -129,11 +130,11 @@ class NeptuneStack(Stack):
             container_name='server',
             command=['npm', 'run', 'server'],
             image=docker_image,
-           # port_mappings=[
-           #     PortMapping(
-           #         container_port=3001
-           #     )
-           # ],
+            port_mappings=[
+                PortMapping(
+                    container_port=3001
+                )
+            ],
             environment={
                 'NEPTUNE_ENDPOINT': write_address,
                 'NODE_ENV': 'production',
@@ -172,23 +173,73 @@ class NeptuneStack(Stack):
                 ApplicationTargetProps(
                     container_port=3000
                 ),
-                ApplicationTargetProps(
-                    container_port=3001,
-                    path_pattern="/server/*",
-                    priority=10
-                )
             ],
         )
 
-        fargate.target_groups[1].configure_health_check(
+        server_target = fargate.service.load_balancer_target(
+            container_name='server',
+            container_port=3001,
+        )
+
+        server_target_group = ApplicationTargetGroup(
+            self,
+            'ServerTargetGroup',
+            targets=[server_target],
+            port=80,
+            vpc=vpcs.default_vpc,
+        )
+
+        server_target_group.configure_health_check(
             path='/server/hello',
         )
+        
+        auth0_secret = Secret.from_secret_complete_arn(
+           self,
+           'Auth0Secret',
+            secret_complete_arn='arn:aws:secretsmanager:us-west-2:618537831167:secret:test/auth0secret/graphviz-5CroJK',
+        )
+
+        fargate.listeners[0].add_action(
+            'AuthAction',
+            action=ListenerAction.authenticate_oidc(
+                authorization_endpoint=auth0_secret.secret_value_from_json('AUTH_URL').unsafe_unwrap(),
+                client_id=auth0_secret.secret_value_from_json('CLIENT_ID').unsafe_unwrap(),
+                client_secret=auth0_secret.secret_value_from_json('CLIENT_SECRET'),
+                issuer=auth0_secret.secret_value_from_json('ISSUER').unsafe_unwrap(),
+                token_endpoint=auth0_secret.secret_value_from_json('TOKEN_URL').unsafe_unwrap(),
+                user_info_endpoint=auth0_secret.secret_value_from_json('USER_INFO_URL').unsafe_unwrap(),
+                next=ListenerAction.forward([fargate.target_groups[0]])
+            )
+        )
+
+        fargate.listeners[0].add_action(
+            'AuthAction',
+            action=ListenerAction.authenticate_oidc(
+                authorization_endpoint=auth0_secret.secret_value_from_json('AUTH_URL').unsafe_unwrap(),
+                client_id=auth0_secret.secret_value_from_json('CLIENT_ID').unsafe_unwrap(),
+                client_secret=auth0_secret.secret_value_from_json('CLIENT_SECRET'),
+                issuer=auth0_secret.secret_value_from_json('ISSUER').unsafe_unwrap(),
+                token_endpoint=auth0_secret.secret_value_from_json('TOKEN_URL').unsafe_unwrap(),
+                user_info_endpoint=auth0_secret.secret_value_from_json('USER_INFO_URL').unsafe_unwrap(),
+                next=ListenerAction.forward([server_target_group])
+            ),
+            conditions=[
+                ListenerCondition.path_patterns(
+                    [
+                        '/server/*'
+                    ]
+                )
+            ],
+            priority=10,
+        )
+
 
         CfnOutput(
             self,
             'NeptuneWriteEndpoint',
             value=write_address,
         )
+
 
 neptune_stack = NeptuneStack(
     app,
